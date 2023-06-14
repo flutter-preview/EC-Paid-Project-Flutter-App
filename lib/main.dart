2d45878a61f6a37d90fd6c827cccffb92266939f
Mohib Qureshi <qureshimohib819@gmail.com>
2023-06-15 01:07:34 +0500

    adjust main

--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,7 @@ class MyApp extends StatelessWidget {
           cupertino: (_, __) => CupertinoAppData(
             theme: MyTheme.iosThemeData,
           ),
-          home:HomePage(),
+          home:Splash(),
           routes: {
             "/splash": (context) => Splash(),
             "/login": (context) => LoginPage(),
